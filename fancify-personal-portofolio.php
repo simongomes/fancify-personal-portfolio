5296e2969ac3b3eaf3652671a2bc6cee8104b2de
Simon Gomes <busy.s.simon@gmail.com>
2024-11-25 03:04:31 +0800

    chore: register script and styles for the blocks

--- fancify-personal-portofolio.php
+++ fancify-personal-portofolio.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * Plugin Name:       Fancify Personal Portofolio
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       WordPress Gutenberg block to display personal portofolio.
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Simon Gomes
+ * Author URI:        https://github.com/simongomes
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       fancify-personal-portofolio
@@ -28,3 +29,39 @@ function create_block_fancify_personal_portofolio_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_fancify_personal_portofolio_block_init' );
+
+
+/**
+ * Enqueues the necessary assets for the Fancify Personal Portfolio plugin.
+ *
+ * This function is responsible for loading the CSS and JavaScript files
+ * required by the Fancify Personal Portfolio plugin to function properly.
+ *
+ * @return void
+ */
+function fancify_personal_portfolio_enqueue_assets() {
+    // Enqueue the bundled JavaScript file.
+    wp_enqueue_script(
+        'fancify-personal-portfolio-blocks',
+        plugins_url( 'build/index.js', __FILE__ ),
+        array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' ),
+        filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' )
+    );
+
+    // Enqueue the editor-only styles.
+    wp_enqueue_style(
+        'fancify-personal-portfolio-blocks-editor',
+        plugins_url( 'build/editor.css', __FILE__ ),
+        array( 'wp-edit-blocks' ),
+        filemtime( plugin_dir_path( __FILE__ ) . 'build/editor.css' )
+    );
+
+    // Enqueue the front-end styles.
+    wp_enqueue_style(
+        'fancify-personal-portfolio-blocks',
+        plugins_url( 'build/style.css', __FILE__ ),
+        array(),
+        filemtime( plugin_dir_path( __FILE__ ) . 'build/style.css' )
+    );
+}
+add_action( 'enqueue_block_assets', 'fancify_personal_portfolio_enqueue_assets' );
